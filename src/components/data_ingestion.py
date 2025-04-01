56b0613a03009dcf4fdc4ae249ecda1ac3b3f42f
Manel Mahroug <manelmahroug@Manels-MacBook-Pro.local>
2025-04-01 15:48:16 -0700

    added data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,6 +1,8 @@
 import os
 import sys
 from src.exception import CustomException
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 from src.logger import logging
     
@@ -47,7 +49,9 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj= DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data= obj.initiate_data_ingestion()
+    data_transformation= DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
 
     
 
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -6,6 +6,7 @@ from dataclasses import dataclass
 
 from src.exception import CustomException
 from src.logger import logging
+from src.utils import save_object
 
     
 import pandas as pd
@@ -83,7 +84,18 @@ class DataTransformation:
             input_feature_train_arr= preprocessing_obj.fit_transform(input_feature_train_df)
             input_feature_test_arr= preprocessing_obj.transform(input_feature_test_df)
 
-            train_arr= np.c_(input_feature_train_arr, np.array(target_feature_train_df))
-        except:
-                pass
+            train_arr= np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+            test_arr= np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+            logging.info("Saving preprocessing object")
+
+            save_object(
+                file_path= self.datat_transformation_config.preprocessor_obj_file_path,
+                obj= preprocessing_obj
+            )
+
+            return (train_arr,
+                    test_arr,
+                    self.datat_transformation_config.preprocessor_obj_file_path)
+        except Exception as e:
+            raise CustomException(e,sys)
             
